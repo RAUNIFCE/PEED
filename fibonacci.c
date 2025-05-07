ef236ff5ec527d5cfbd900411b8c9dc276db91d0
RUANIFCE <ruan.goncalves08@aluno.ifce.edu.br>
2025-05-07 20:34:48 -0300

    fibonacci

---
+++ fibonacci.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+int fibo(int n){
+
+    if(n==0){
+        return 0;
+    }
+    if(n==1){
+        return 1;
+    }
+    return fibo(n-1)+fibo(n-2);
+}
+int main()
+{
+    int result=fibo(9);
+    printf("%d",result);
+    return 0;
+}
