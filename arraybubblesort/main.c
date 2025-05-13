1cd6e5270f1c8080548887e4279df9d090b6a98d
RUANIFCE <ruan.goncalves08@aluno.ifce.edu.br>
2025-05-12 21:06:27 -0300

    correção da atividade
    errei no metodo de comparação

--- arraybubblesort/main.c
+++ arraybubblesort/main.c
@@ -20,28 +20,28 @@ int main()
     printf("\ndigite 1 para crescente e 2 descrescente\n");
     scanf("%d",&codigo);
     if(codigo==1){
-        for(int i=0;i<tamanho;i++){
-            for(int j=0;j<tamanho;j++){
-                if(array[i]<array[j]){
+        for(int i=0;i<tamanho-1;i++){
+            for(int j=0;j<tamanho-1-i;j++){
+                if(array[j]>array[j+1]){
 
                     int temp;
-                    temp=array[i];
-                    array[i]=array[j];
-                    array[j]=temp;
+                    temp=array[j];
+                    array[j]=array[j+1];
+                    array[j+1]=temp;
 
                 }
             }
         }
     }
     if(codigo==2){
-        for(int i=0;i<tamanho;i++){
-            for(int j=0;j<tamanho;j++){
-                if(array[i]>array[j]){
+        for(int i=0;i<tamanho-1;i++){
+            for(int j=0;j<tamanho-1-i;j++){
+                if(array[j]<array[j+1]){
 
                     int temp;
-                    temp=array[i];
-                    array[i]=array[j];
-                    array[j]=temp;
+                    temp=array[j];
+                    array[j]=array[j+1];
+                    array[j+1]=temp;
 
                 }
             }
